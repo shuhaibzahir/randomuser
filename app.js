58d90e437fdb91ccfab7ede30b06b62b931bf1cd
shuhaibzahir <yesshuhaib.ss@gmail.com>
2022-01-21 08:46:57 +0530

    updated fetch with async and await

--- app.js
+++ app.js
@@ -7,21 +7,19 @@ data(){
         email:'dev.shuahib@gmail.com',
         picture:'https://pbs.twimg.com/profile_images/1390886031851855880/wktTnTP3_400x400.jpg'
     }
-},
+},  
 methods:{
-    getRandomUsers(){
+   async getRandomUsers(){
 
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then(result => {
-            let data = result.results[0]
-            this.firstName=data.name.first;
-            this.lastName = data.name.last;
-            this.email = data.email
-            this.picture = data.picture.large 
-            this.gender = data.gender
-
-        });
+        let data = await fetch('https://randomuser.me/api/').then((respo)=>respo.json())
+        data= data.results[0]
+        this.gender=data.gender,
+        this.firstName=data.name.first,
+        this.lastName=data.name.last,
+        this.email=data.email,
+        this.picture=data.picture.large
+         
+         
     }
 }
 })
